a986206afcce5b6073b5b77c9897446730b99788
Alan King <alanking@renci.org>
2022-03-28 14:03:15 -0400

    [#2042] Remove use of bzero

--- s3/s3_operations.cpp
+++ s3/s3_operations.cpp
@@ -1100,7 +1100,7 @@ namespace irods_s3 {
             // get ref to fco
             irods::data_object_ptr object = boost::dynamic_pointer_cast<irods::data_object>(_ctx.fco());
 
-            bzero (_statbuf, sizeof (struct stat));
+            std::memset(_statbuf, 0, sizeof(struct stat));
 
             boost::filesystem::path p(object->physical_path());
 
@@ -1123,8 +1123,7 @@ namespace irods_s3 {
                         std::string region_name = get_region_name(_ctx.prop_map());
 
                         callback_data_t data;
-                        S3BucketContext bucketContext;
-                        bzero(&bucketContext, sizeof(bucketContext));
+                        S3BucketContext bucketContext{};
 
                         bucketContext.bucketName = bucket.c_str();
                         bucketContext.protocol = s3GetProto(_ctx.prop_map());
@@ -1137,7 +1136,7 @@ namespace irods_s3 {
                         S3ResponseHandler headObjectHandler = { &responsePropertiesCallback, &responseCompleteCallbackIgnoreLoggingNotFound};
                         std::size_t retry_cnt = 0;
                         do {
-                            bzero (&data, sizeof (data));
+                            std::memset(&data, 0, sizeof(data));
                             std::string&& hostname = s3GetHostname(_ctx.prop_map());
                             bucketContext.hostName = hostname.c_str();
                             data.pCtx = &bucketContext;
