8f3467d9ee1c0d50c5ff41c6c7e8d6ef938ac65c
Justin James <jjames@renci.org>
2023-11-30 17:35:15 -0500

    [#2151][#2153] Stat cache file if it is open
    If there is an open cache file, stat it instead of doing a HEAD to S3.
    Fix translation of open mode to posix stream.

--- s3/s3_transport/include/s3_transport.hpp
+++ s3/s3_transport/include/s3_transport.hpp
@@ -445,7 +445,6 @@ namespace irods::experimental::io::s3_transport
                         last_file_to_close_, data.know_number_of_threads, data.threads_remaining_to_close);
 
                 // if a critical error occurred - do not flush cache file or complete multipart upload
-
                 if (!this->error_.ok()) {
 
                     return_value = false;
@@ -836,6 +835,30 @@ namespace irods::experimental::io::s3_transport
         int64_t get_existing_object_size() {
             return existing_object_size_;
         }
+
+        bool is_cache_file_open() {
+
+            if (!use_cache_) {
+                return false;
+            }
+
+            named_shared_memory_object shm_obj{shmem_key_,
+                config_.shared_memory_timeout_in_seconds,
+                constants::MAX_S3_SHMEM_SIZE};
+
+            return shm_obj.atomic_exec([](auto& data) -> bool
+            {
+                return !data.cache_file_flushed;
+            });
+
+        }
+
+        std::string get_cache_file_path() {
+            namespace bf = boost::filesystem;
+            bf::path cache_file =  bf::path(config_.cache_directory) / bf::path(object_key_ + "-cache");
+            return cache_file.string();
+        }
+
 
     private:
 
@@ -1401,6 +1424,7 @@ namespace irods::experimental::io::s3_transport
                 }
 
                 if (object_status == object_s3_status::IN_S3 && this->download_to_cache_) {
+                    rodsLog(config_.developer_messages_log_level, "%s:%d (%s) [[%lu]] Downloading object to cache", __FILE__, __LINE__, __FUNCTION__, this->get_thread_identifier());
 
                     cache_file_download_status download_status = this->download_object_to_cache(shm_obj, s3_object_size);
 
@@ -1458,8 +1482,15 @@ namespace irods::experimental::io::s3_transport
                         }
 
                         if (!cache_fstream_ || !cache_fstream_.is_open()) {
-                            rodsLog(LOG_ERROR, "%s:%d (%s) [[%lu]] Failed to open cache file %s, error=%s\n",
-                                    __FILE__, __LINE__, __FUNCTION__, this->get_thread_identifier(), cache_file_path_.c_str(), strerror(errno));
+                            rodsLog(LOG_ERROR, "%s:%d (%s) [[%lu]] Failed to open cache file %s, error=%s open_mode: [app=%d][binary=%d][in=%d][out=%d][trunc=%d][ate=%d]",
+                                    __FILE__, __LINE__, __FUNCTION__, this->get_thread_identifier(), cache_file_path_.c_str(), strerror(errno),
+                                    (mode & std::ios::app) != 0,
+                                    (mode & std::ios::binary) != 0,
+                                    (mode & std::ios::in) != 0,
+                                    (mode & std::ios::out) != 0,
+                                    (mode & std::ios::trunc) != 0,
+                                    (mode & std::ios::ate) != 0
+                                    );
                             this->set_error(ERROR(UNIX_FILE_OPEN_ERR, "Failed to open S3 cache file"));
                             return_value = false;
                             return;
