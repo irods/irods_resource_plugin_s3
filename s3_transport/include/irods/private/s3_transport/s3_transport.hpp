9d9c0cd4127d05588e3fbcf0772b1f117319de12
Justin James <jjames@renci.org>
2025-04-10 19:53:42 -0400

    [2241] code review changes

--- s3_transport/include/irods/private/s3_transport/s3_transport.hpp
+++ s3_transport/include/irods/private/s3_transport/s3_transport.hpp
@@ -1481,8 +1481,8 @@ namespace irods::experimental::io::s3_transport
 
 						// If we know the number of threads, use threads_remaining to close to determine if this is the
 						// first open. If we do not know the number of threads, use file_open_counter.
-						if ((data.know_number_of_threads && data.threads_remaining_to_close == 0) ||
-						    (!(data.know_number_of_threads) && data.file_open_counter == 0))
+						if ((data.know_number_of_threads && 0 == data.threads_remaining_to_close) ||
+						    (!data.know_number_of_threads && 0 == data.file_open_counter))
 						{
 							trunc_flag = true;
                             mode = mode_;
