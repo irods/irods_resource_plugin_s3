8cb35926a1bbc7d8e5a123d2dfa083ea5465b136
Kory Draughn <korydraughn@renci.org>
2026-03-20 16:41:29 -0400

    [irods/irods_development_environment#165] Add option to build with release packages

--- irods_consortium_continuous_integration_build_hook.py
+++ irods_consortium_continuous_integration_build_hook.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 import glob
 import multiprocessing
 import optparse
@@ -78,9 +76,12 @@ def copy_output_packages(build_directory, output_root_directory):
         irods_python_ci_utilities.append_os_specific_directory(output_root_directory),
         lambda s:s.endswith(irods_python_ci_utilities.get_package_suffix()))
 
-def main(build_directory, output_root_directory, irods_packages_root_directory, externals_directory):
+def main(build_directory, output_root_directory, irods_packages_root_directory, externals_directory, irods_package_version):
     install_building_dependencies(externals_directory)
-    if irods_packages_root_directory:
+    if irods_package_version is not None:
+        irods_python_ci_utilities.install_irods_packages_repository()
+        irods_python_ci_utilities.install_released_irods_dev_and_runtime_packages(irods_package_version)
+    elif irods_packages_root_directory:
         irods_python_ci_utilities.install_irods_dev_and_runtime_packages(irods_packages_root_directory)
     build_directory = os.path.abspath(build_directory or tempfile.mkdtemp(prefix='irods_s3_plugin_build_directory'))
     irods_python_ci_utilities.subprocess_get_output(['cmake', os.path.dirname(os.path.realpath(__file__))], check_rc=True, cwd=build_directory)
@@ -94,9 +95,11 @@ if __name__ == '__main__':
     parser.add_option('--output_root_directory')
     parser.add_option('--irods_packages_root_directory')
     parser.add_option('--externals_packages_directory')
+    parser.add_option('--irods_package_version')
     options, _ = parser.parse_args()
 
     main(options.build_directory,
          options.output_root_directory,
          options.irods_packages_root_directory,
-         options.externals_packages_directory)
+         options.externals_packages_directory,
+         options.irods_package_version)
